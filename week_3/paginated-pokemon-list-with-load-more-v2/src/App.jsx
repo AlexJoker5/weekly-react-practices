e395374998b0a3a3645b8b6e97f92d49e6cf1b14
Christopher Lone Toe <christikai2001@gmail.com>
2026-07-06 16:35:27 +0630

    style the pokemon list

--- week_3/paginated-pokemon-list-with-load-more-v2/src/App.jsx
+++ week_3/paginated-pokemon-list-with-load-more-v2/src/App.jsx
@@ -4,7 +4,7 @@ import Pokemon from './components/Pokemon';
 
 function App() {
 
-  const ITEMS_PER_PAGE = 5;
+  const ITEMS_PER_PAGE = 12;
 
   const [pokemon, setPokemon] = useState([]);
   const [offset, setOffset] = useState(0);
@@ -20,13 +20,13 @@ function App() {
       const data = await response.json();
 
       // const urls = data?.results.map((p) => p.url);
-      console.log("data: ", data)
+      // console.log("data: ", data)
       const count = data?.count || 0;
       if(isMounted) {
         setTotalCount(count);
         setPokemon((prev) => [...prev, ...(data?.results || [])]);
       }
-      console.log("Is Mounted: ", isMounted);
+      // console.log("Is Mounted: ", isMounted);
     }
     fetchPokemons();
     return () => {
@@ -47,19 +47,20 @@ function App() {
   
 
   return (
-    <div className='p-5'>
-      <ul className='listitem'>
+    <div className='p-5 mt-10 mx-50'>
+      <div className='grid grid-cols-4 gap-5'>
         {pokemon.length > 0 && (
-          
           pokemon.map((poke, index) => {
             return <Pokemon name={poke.name} url={poke.url} key={index} />
           })
         )}
-      </ul>
-      <p>Displaying {pokemon.length} of {totalCount} results</p>
-      <p>Has More: {hasMore ? "Hello" : "World"}</p>
+      </div>
+      {/* <p>Displaying {pokemon.length} of {totalCount} results</p> */}
+      {/* <p>Has More: {hasMore ? "Hello" : "World"}</p> */}
       {shouldShowButton && (
-        <button onClick={() => clickHandler()} className='mt-5 px-3 py-2 bg-sky-500 hover:bg-sky-600 rounded-lg text-white'>Load more</button>
+        <div className='flex justify-center mt-5'>
+          <button onClick={() => clickHandler()} className='mt-5 px-3 py-2 bg-cyan-600 opacity-80 hover:bg-cyan-700 rounded-lg text-white'>Load more Pokémon</button>
+        </div>
       )}
     </div>
   )
